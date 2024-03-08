fa7adf5047a52b77bd3363353933d09d1cfcc3dd
tachu105 <dragontree105@gmail.com>
2024-03-08 23:22:22 +0900

    コントローラーとモデルを追加．~/postsでDBを表示できるようにした．

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Post一覧を表示する
+     * @param Post Postモデル
+     * @return array Postモデルリスト
+     */
+    public function index(Post $post){
+        return $post -> get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+/**
+ * /postsにアクセスした際にPostControllerのindexメソッドを呼び出す
+ */
+// Route::get('/posts', function (Request $request) {
+//     return PostController::index();
+// });
+//意味合い的にはこんな感じ（これだとダメ）だけど，さっきのを使うとLaravalが必要なパラメータとかを
+//自動で渡してくれるので，手動で明記しなくても良く，ミスが減るため，こっちを使うことが推奨．
+
+
+
